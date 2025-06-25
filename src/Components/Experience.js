d6e960f3f5f0f854944080515842aea661380be5
Mohammed2327 <mohammedmujju327@gmail.com>
2025-06-25 13:32:11 +0530

    Updated the exp

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -10,60 +10,67 @@ const Experience = () => {
   };
 
   return (
-    <>
-      <div className="container exp" id="experience">
-        <h1> Experience</h1>
-        {experience.map((data, index) => {
-          const isActive = activeIndex === index;
-          return (
-            <div
-              key={data.id}
-              className="exp-items text-center my-5"
-              data-aos="zoom-in"
-              data-aos-duration="1000"
-              onClick={() => handleToggle(index)}
-            >
-              <div className="left">
-                <LazyLoad height={200} offset={100}>
-                  <img
-                    src={`${process.env.PUBLIC_URL}/assets/${data.imgSrc}`}
-                    alt="Profile"
-                    className="logo"
-                  />
-                </LazyLoad>
-              </div>
-              <div className="right">
-                <h2>
-                  {" "}
-                  {data.role} {"- "}
-                  {data.organisation} {data.client}
-                </h2>
+    <div className="container exp" id="experience">
+      <h1 style={{ textAlign: "center", marginBottom: "30px", color: "gold" }}>
+        Experience
+      </h1>
+      {experience.map((data, index) => {
+        const isActive = activeIndex === index;
 
-                <h4 className="job-details">
-                  <span style={{ color: "yellowgreen" }}>
-                    {data.startDate} {" - "}
-                    {data.endDate}{" "}
-                  </span>{" "}
-                  <span style={{ color: "yellow" }}> {data.location} </span>
-                </h4>
-                {!isActive && (
-                  <p className="roles-responsibilities">
-                    Click here for Roles and Responsibilities
-                  </p>
-                )}
-                {isActive && data.responsibilities.length > 0 && (
-                  <ul style={{ color: "white" }}>
-                    {data.responsibilities.map((resp, respIndex) => (
-                      <li key={respIndex}>{resp.description}</li>
-                    ))}
-                  </ul>
-                )}
-              </div>
+        return (
+          <div
+            key={data.id}
+            className="exp-items"
+            data-aos="zoom-in"
+            data-aos-duration="1000"
+            onClick={() => handleToggle(index)}
+          >
+            {/* Left - Logo */}
+            <div className="left">
+              <LazyLoad height={100} offset={100}>
+                <img
+                  src={`${process.env.PUBLIC_URL}/assets/${data.imgSrc}`}
+                  alt={`${data.organisation} Logo`}
+                  className="logo"
+                />
+              </LazyLoad>
             </div>
-          );
-        })}
-      </div>
-    </>
+
+            {/* Right - Role, Org, Date, Location, Responsibilities */}
+            <div className="right" style={{ flex: 1, paddingLeft: "40px", marginLeft: "20px" }}>
+              <h2 style={{ marginBottom: "10px", color: "white" }}>
+                {data.role} - {data.organisation}
+                {data.client && (
+                  <span style={{ fontSize: "0.9rem", color: "#ccc" }}>
+                    {" "}
+                    ({data.client})
+                  </span>
+                )}
+              </h2>
+
+              <h4 className="job-details">
+                <span style={{ color: "yellowgreen", fontWeight: 600 }}>
+                  {data.startDate} - {data.endDate}
+                </span>{" "}
+                | <span style={{ color: "yellow" }}>{data.location}</span>
+              </h4>
+
+              {!isActive ? (
+                <p className="roles-responsibilities">
+                  Click here for Roles and Responsibilities
+                </p>
+              ) : (
+                <ul>
+                  {data.responsibilities.map((item, i) => (
+                    <li key={i}>{item.description}</li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 

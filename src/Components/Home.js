3d7853bef6505eea9a33a4b93bb8faf3d154fea5
Mohammed2327 <mohammedmujju327@gmail.com>
2025-10-15 16:22:53 +0530

    updated the resume with skills

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,17 +2,16 @@ import React, { useEffect, useRef } from "react";
 import Resume from "../Resume/MOHAMMED MUJEEBUDDIN.pdf";
 import profile from "./Data/profile.json";
 import Typed from "typed.js";
-import LazyLoad from 'react-lazy-load';
-
+import LazyLoad from "react-lazy-load";
 
 const Home = () => {
   const typedRef = useRef(null);
   useEffect(() => {
     const options = {
       strings: [
-        "Welcome to my Portfolio", 
-        "I'm Mohammed Mujeebuddin", 
-        "I'm Frontend Developer (ReactJS)",
+        "Welcome to my Portfolio",
+        "I'm Mohammed Mujeebuddin",
+        "A Software Engineer II",
       ],
       typeSpeed: 50,
       backSpeed: 50,
@@ -30,34 +29,29 @@ const Home = () => {
   return (
     <>
       <div className="container home" id="home">
-        <div className="left"
-        data-aos= "fade-up-right"
-    data-aos-duration="1000"
-        >
+        <div className="left" data-aos="fade-up-right" data-aos-duration="1000">
           <h1 ref={typedRef}> </h1>
           <a
             href={Resume}
             download="MOHAMMED-MUJEEBUDDIN.pdf"
             className="btn btn-outline-warning my-3"
           >
-          Download Resume
+            Download Resume
           </a>
         </div>
         <div className="right">
-          <div className="img"
-          data-aos= "fade-up-left"
-          data-aos-duration="1000">
-            
+          <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
             <LazyLoad height={-70} offset={100}>
-            <img src={`${process.env.PUBLIC_URL}/assets/${profile.imgSrc}`}
-            alt="Profile" />
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/${profile.imgSrc}`}
+                alt="Profile"
+              />
             </LazyLoad>
-
           </div>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default Home;
